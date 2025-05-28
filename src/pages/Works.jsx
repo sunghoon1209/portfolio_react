b5aac3dee54d3fa46baa122e0f97e3a91f10beda
주성훈 <83044@naver.com>
2025-05-28 23:18:55 +0900

    card

--- src/pages/Works.jsx
+++ src/pages/Works.jsx
@@ -1,10 +1,28 @@
 import styled from "styled-components"
 
+const StyledWorksSection = styled.section`
+    padding:20px 0;
+    width: 100%;
+    display: flex;
+    flex-wrap: wrap;
+    gap:  2%;
+`
+const StyledCard = styled.div`
+    width: 32%;
+    aspect-ratio: 16/9;
+    background: #fff;
+`
+
 
 const Works = ()=>{
     return(
         <>
-            1
+            <StyledWorksSection>
+                <StyledCard>1</StyledCard>
+                <StyledCard>1</StyledCard>
+                <StyledCard>1</StyledCard>
+                <StyledCard>1</StyledCard>
+            </StyledWorksSection>
         </>
     )
 }
